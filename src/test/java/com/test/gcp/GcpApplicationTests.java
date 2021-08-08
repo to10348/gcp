56487bb02a48a98638c72bf5f3a8bfc749644ca8
woojin.shin <woojin.shin@nhnsoft.com>
2021-08-08 16:06:34 +0900

    Initial commit

---
+++ src/test/java/com/test/gcp/GcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.gcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
