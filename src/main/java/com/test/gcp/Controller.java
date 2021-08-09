eca6f0183f9da6e38a628a7fc8d08fb1c03fd034
woojin.shin <woojin.shin@nhnsoft.com>
2021-08-09 23:46:10 +0900

    1

--- src/main/java/com/test/gcp/Controller.java
+++ src/main/java/com/test/gcp/Controller.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping
     public String getTest() {
-        return "success.";
+        return "success.1";
     }
 }
